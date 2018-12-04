a38abbb9e67d5e6ec783fa8dd9054df950300c81
Jackson Dean <jacksonsdean@gmail.com>
2018-12-03 21:17:28 -0600

    new net gets about 18-20% :)

--- train.py
+++ train.py
@@ -8,27 +8,50 @@ TRAIN_SPLIT = 0.8
 train_size = int(N_SAMPLES * TRAIN_SPLIT)
 test_size = N_SAMPLES - train_size
 
-
 if __name__ == '__main__':
     # Our net:
     labels = np.load("labels.npy")
     data = np.load("data.npy")
+    data = np.ndarray.astype(data, "float32")
+    labels = np.ndarray.astype(labels, "float32")
 
+    shaped_data = np.array(data)
+    shaped_labels = labels
+
+    # for d in data:
+    #     d1 = d.reshape(96,1366)
+    #     shaped_data.append(d1)
+
+    shaped_data = np.array(shaped_data)
+
+    # shaped_data = tf.contrib.layers.batch_norm(shaped_data)
+    # shaped_labels = tf.contrib.layers.batch_norm(labels)
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(),
+        tf.keras.layers.Flatten(input_shape=(96, 1366)),
         tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(256, activation=tf.nn.relu),
+        tf.keras.layers.Dense(128, activation=tf.nn.relu),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
     model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
+                  loss='categorical_crossentropy', # WHY DID WE DO THIS??
                   metrics=['accuracy'])
 
-    x_train = data[0:train_size]
-    x_test = data[train_size:]
 
-    y_train = labels[0:train_size]
-    y_test = labels[train_size:]
 
-    model.fit(x_train, y_train, epochs=N_EPOCH)
+    shaped_data = shaped_data.reshape(1000, 96, 1366)
+    shaped_labels = shaped_labels.reshape(1000, 10)
+
+    x_train = shaped_data[0:train_size]
+    x_test = shaped_data[train_size:]
+
+    y_train = shaped_labels[0:train_size]
+    y_test = shaped_labels[train_size:]
+
+
+    print("x:", x_train.shape)
+    print("y:", len(y_train))
+
+    model.fit(x_train, y_train, epochs=N_EPOCH)#, steps_per_epoch=800)
     model.evaluate(x_test, y_test)
